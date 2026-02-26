2ad881df09859ef4ac5d2440d5b57427552dbafa
queen-programmer1 <angifaith16@gmail.com>
2026-02-26 11:55:33 -0500

    Added a counter to keep track of how many times the dice is rolled - will print this info once the program has ended

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+#Dice Roller Game - a program that simulates rolling 2 dice 
+#The user has to enter "y" to roll the dice and "n" to end the game
+#The program continues to run even when the user inputs an "invalid choice", 
+#The program only terminates once the user enter "n", ending the game
+
 import random
 
+dice_rolled_counter = 0 
 userInput = input("Roll the dice? (y/n): ")
 
 
@@ -7,9 +13,12 @@ while True:
     if userInput == "y":
         print((random.randrange(1,6), random.randrange(1,6)))
         userInput = input("Roll the dice? (y/n): ")
+        dice_rolled_counter = dice_rolled_counter + 1
+        
         
     elif userInput == "n":
         print("Thanks for playing! :)")
+        print("You rolled the dice " + str(dice_rolled_counter) + " times during this game.")
         break
     
     else:
